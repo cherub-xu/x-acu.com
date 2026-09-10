9304b95c4447a81042c513ef61f192b138521bec
cherub-xu <cherub.xu@gmail.com>
2026-09-10 17:25:36 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-
+ 
     // Test endpoint
     if (url.pathname === "/api/test") {
       return Response.json({
